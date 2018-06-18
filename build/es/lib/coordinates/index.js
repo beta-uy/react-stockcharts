8233038d466697cd0fbcbbb0d47b045491be2838
Martín Osimani <hello@martinosimani.co>
2018-06-18 14:55:32 -0300

    Drops build folder in favour of prepare script

--- build/es/lib/coordinates/index.js
+++
@@ -1,9 +0,0 @@
-export { default as EdgeIndicator } from "./EdgeIndicator";
-export { default as CurrentCoordinate } from "./CurrentCoordinate";
-export { default as MouseCoordinateX } from "./MouseCoordinateX";
-export { default as MouseCoordinateY } from "./MouseCoordinateY";
-export { default as CrossHairCursor } from "./CrossHairCursor";
-export { default as PriceCoordinate } from "./PriceCoordinate";
-export { default as AxisXCursor } from "./AxisXCursor";
-export { default as AxisYCenteredTitle } from "./AxisYCenteredTitle";
-//# sourceMappingURL=index.js.map
